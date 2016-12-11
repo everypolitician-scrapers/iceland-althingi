44828aee53f9d1f3699abe9eaf603f078b5c8b29
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-11 11:43:06 +0000

    Archive Icelandic member pages from Members, Alternates and Substitutes pages
    The Icelandic version of the site covers alternates and substitutes
    as well as the members listed on the English page.

--- scraper.rb
+++ scraper.rb
@@ -5,10 +5,16 @@ require 'scraped_page_archive/open-uri'
 
 require_rel 'lib'
 
-url = 'http://www.althingi.is/altext/cv/en/'
-MembersPage.new(response: Scraped::Request.new(url: url).response)
-           .member_urls
-           .each do |url|
-             Scraped::HTML.new(response: Scraped::Request.new(url: url).response)
-             puts "Opening: #{url}"
-           end
+# url = 'http://www.althingi.is/altext/cv/en/'
+urls = ['http://www.althingi.is/thingmenn/althingismenn/', # members
+        'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/', # alternates
+        'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'] # substitues
+
+urls.each do |url|
+  IcelandicMembersPage.new(response: Scraped::Request.new(url: url).response)
+             .member_urls
+             .each do |url|
+               puts "Opening: #{url}"
+               Scraped::HTML.new(response: Scraped::Request.new(url: url).response)
+             end
+end

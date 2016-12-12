f243089ff048016ce5c6f20a75479f295d055144
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-12 11:53:40 +0000

    Scrape both English and Icelandic pages

--- scraper.rb
+++ scraper.rb
@@ -5,17 +5,28 @@ require 'scraped_page_archive/open-uri'
 
 require_rel 'lib'
 
-# url = 'http://www.althingi.is/altext/cv/en/'
+english_url = 'http://www.althingi.is/altext/cv/en/'
 icelandic_urls = {
         members: 'http://www.althingi.is/thingmenn/althingismenn/',
         alternates: 'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/',
         substitues: 'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'
         }
 
+def scraped_page_for(url)
+  puts "Opening: #{url}"
+  Scraped::HTML.new(response: Scraped::Request.new(url: url).response)
+end
+
+EnglishMembersPage.new(response: Scraped::Request.new(url: english_url).response)
+                  .member_urls
+                  .each do |url|
+                    scraped_page_for(url)
+                  end
+
 icelandic_urls.each do |key, url|
   IcelandicMembersPage.new(response: Scraped::Request.new(url: url).response)
              .member_urls
              .each do |url|
-               Scraped::HTML.new(response: Scraped::Request.new(url: url).response)
+               scraped_page_for(url)
              end
 end

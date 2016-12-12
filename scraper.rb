36692c14a631ea85541b217320280f0f7c146056
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-12 11:52:46 +0000

    Scrape both English and Icelandic pages

--- scraper.rb
+++ scraper.rb
@@ -5,16 +5,25 @@ require 'scraped_page_archive/open-uri'
 
 require_rel 'lib'
 
-# url = 'http://www.althingi.is/altext/cv/en/'
-urls = ['http://www.althingi.is/thingmenn/althingismenn/', # members
-        'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/', # alternates
-        'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'] # substitues
+members_pages = {
+  EnglishMembersPage => ['http://www.althingi.is/altext/cv/en/'],
+  IcelandicMembersPage => [
+    'http://www.althingi.is/thingmenn/althingismenn/',
+    'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/',
+    'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'
+  ]
+}
 
-urls.each do |url|
-  IcelandicMembersPage.new(response: Scraped::Request.new(url: url).response)
-             .member_urls
-             .each do |url|
-               puts "Opening: #{url}"
-               Scraped::HTML.new(response: Scraped::Request.new(url: url).response)
-             end
+def scraped_page_for(url, klass = Scraped::HTML)
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
+
+members_pages.each do |key, page_urls|
+  page_urls.each do |page_url|
+    scraped_page_for(page_url, key)
+      .member_urls
+      .each do |member_url|
+      _ = scraped_page_for(member_url)
+    end
+  end
 end

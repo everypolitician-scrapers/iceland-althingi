a4209c840e8d3fa7f796768f455ef84185dcfdb9
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-12 11:52:46 +0000

    Store Icelandic members page urls in hash
    Replaces need for comments.

--- scraper.rb
+++ scraper.rb
@@ -6,15 +6,16 @@ require 'scraped_page_archive/open-uri'
 require_rel 'lib'
 
 # url = 'http://www.althingi.is/altext/cv/en/'
-urls = ['http://www.althingi.is/thingmenn/althingismenn/', # members
-        'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/', # alternates
-        'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'] # substitues
+icelandic_urls = {
+        members: 'http://www.althingi.is/thingmenn/althingismenn/',
+        alternates: 'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/',
+        substitues: 'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'
+        }
 
-urls.each do |url|
+icelandic_urls.each do |key, url|
   IcelandicMembersPage.new(response: Scraped::Request.new(url: url).response)
              .member_urls
              .each do |url|
-               puts "Opening: #{url}"
                Scraped::HTML.new(response: Scraped::Request.new(url: url).response)
              end
 end

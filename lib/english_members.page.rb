0e285f0ae6168d27aeb15eb03ea40711fcb8cf67
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-12 11:37:15 +0000

    Rename MembersPage to EnglishMembersPage
    Since we now have IcelandicMembersPage, I have renamed MembersPage
    to ENglishMembersPage to prevent any confusion that this might
    be a base class. This class specifically represents a page listing
    the members of the legislature in English.

--- lib/english_members.page.rb
+++ lib/english_members.page.rb
@@ -1,6 +1,6 @@
 require 'scraped'
 
-class MembersPage < Scraped::HTML
+class EnglishMembersPage < Scraped::HTML
   field :member_urls do
     noko.xpath('//table[@id="members"]/tbody/tr/td/b/a/@href').map do |row|
       URI.join(url,row.text).to_s

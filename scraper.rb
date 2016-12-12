957766f5f871f04f677db94e5be3e80cee0680e6
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-12 16:41:17 +0000

    Loop over URLs in single structure
    This enables us to collapse the two loops for EnglishMembersPage
    and IcelandicMembersPage into one.

--- scraper.rb
+++ scraper.rb
@@ -5,27 +5,25 @@ require 'scraped_page_archive/open-uri'
 
 require_rel 'lib'
 
-english_url = 'http://www.althingi.is/altext/cv/en/'
-icelandic_urls = {
-  members: 'http://www.althingi.is/thingmenn/althingismenn/',
-  alternates: 'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/',
-  substitues: 'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'
+urls = {
+  EnglishMembersPage: [ 'http://www.althingi.is/altext/cv/en/'],
+  IcelandicMembersPage: [
+    'http://www.althingi.is/thingmenn/althingismenn/',
+    'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-sitja-a-althingi/',
+    'http://www.althingi.is/thingmenn/thingmenn/varamenn-sem-hafa-tekid-saeti/'
+  ]
 }
 
 def scraped_page_for(url)
   Scraped::HTML.new(response: Scraped::Request.new(url: url).response)
 end
 
-EnglishMembersPage.new(response: Scraped::Request.new(url: english_url).response)
-                  .member_urls
-                  .each do |url|
-                    _ = scraped_page_for(url)
-                  end
-
-icelandic_urls.each do |key, url|
-  IcelandicMembersPage.new(response: Scraped::Request.new(url: url).response)
-             .member_urls
-             .each do |url|
-               _ = scraped_page_for(url)
-             end
+urls.each do |key, urls|
+  urls.each do |url|
+    eval(key.to_s).new(response: Scraped::Request.new(url: url).response)
+               .member_urls
+               .each do |url|
+                 _ = scraped_page_for(url)
+               end
+  end
 end

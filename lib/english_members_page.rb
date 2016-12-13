5c8d2d83daa191451292541114449b30099ab833
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-13 15:40:07 +0000

    Tidy for Rubocop

--- lib/english_members_page.rb
+++ lib/english_members_page.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 require 'scraped'
 
 class EnglishMembersPage < Scraped::HTML
   field :member_urls do
     noko.xpath('//table[@id="members"]/tbody/tr/td/b/a/@href').map do |row|
-      URI.join(url,row.text).to_s
+      URI.join(url, row.text).to_s
     end
   end
 end

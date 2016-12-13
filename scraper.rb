e1b62c1d523d8e179b1557c39d26ca898cb557e9
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-13 10:34:20 +0000

    Rename variables for Rubocop
    Rubocop was complaining that variables were masking outer scope
    variables with the same name.

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,7 @@ require 'scraped_page_archive/open-uri'
 
 require_rel 'lib'
 
-urls = {
+members_pages = {
   EnglishMembersPage => ['http://www.althingi.is/altext/cv/en/'],
   IcelandicMembersPage => [
     'http://www.althingi.is/thingmenn/althingismenn/',
@@ -18,12 +18,12 @@ def scraped_page_for(url, klass = Scraped::HTML)
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-urls.each do |key, urls|
-  urls.each do |url|
-    scraped_page_for(url, key)
+members_pages.each do |key, _page_urls|
+  urls.each do |page_url|
+    scraped_page_for(page_url, key)
       .member_urls
-      .each do |url|
-      _ = scraped_page_for(url)
+      .each do |member_url|
+      _ = scraped_page_for(member_url)
     end
   end
 end

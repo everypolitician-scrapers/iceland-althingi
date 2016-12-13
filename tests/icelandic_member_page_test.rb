678f8194e96aaebdf7514741f219919d4829ac01
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-13 16:09:01 +0000

    Add tests

---
+++ tests/icelandic_member_page_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require_relative '../lib/icelandic_members_page.rb'
+
+describe IcelandicMembersPage do
+  describe 'member_urls' do
+    it 'should return the expected number of urls' do
+      url = 'http://www.althingi.is/thingmenn/althingismenn/'
+      IcelandicMembersPage.new(response: Scraped::Request.new(url:url).response)
+                        .member_urls
+                        .count
+                        .must_equal 63
+    end
+  end
+
+  describe 'member_urls' do
+    it 'should return an expected url in its result' do
+            url = 'http://www.althingi.is/thingmenn/althingismenn/'
+            IcelandicMembersPage.new(response: Scraped::Request.new(url:url).response)
+                        .member_urls
+                        .must_include 'http://www.althingi.is/altext/cv/is/?nfaerslunr=1265'
+    end
+  end
+end

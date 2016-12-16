ebcefa641b3a2d7dadd78e551fa589f695dc8088
Tony Bowden <tony@tmtm.com>
2016-12-16 10:03:58 +0000

    Extract `subject` responses

--- tests/english_member_page_test.rb
+++ tests/english_member_page_test.rb
@@ -5,23 +5,21 @@ require_relative '../lib/english_members_page.rb'
 describe EnglishMembersPage do
   around { |test| VCR.use_cassette('EnglishMembersPage', &test) }
 
-  url = 'http://www.althingi.is/altext/cv/en/'
+  subject do
+    url = 'http://www.althingi.is/altext/cv/en/'
+    EnglishMembersPage.new(response: Scraped::Request.new(url: url).response)
+  end
 
   describe 'member_urls' do
     it 'should return the expected number of urls' do
-      EnglishMembersPage.new(response: Scraped::Request.new(url: url).response)
-                        .member_urls
-                        .count
-                        .must_equal 63
+      subject.member_urls.count.must_equal 63
     end
   end
 
   describe 'member_urls' do
     it 'should return an expected url in its result' do
-      url = 'http://www.althingi.is/altext/cv/en/'
-      EnglishMembersPage.new(response: Scraped::Request.new(url: url).response)
-                        .member_urls
-                        .must_include 'http://www.althingi.is/altext/cv/en/?nfaerslunr=182'
+      wanted = 'http://www.althingi.is/altext/cv/en/?nfaerslunr=182'
+      subject.member_urls.must_include wanted
     end
   end
 end
--- tests/icelandic_member_page_test.rb
+++ tests/icelandic_member_page_test.rb
@@ -5,25 +5,21 @@ require_relative '../lib/icelandic_members_page.rb'
 describe IcelandicMembersPage do
   around { |test| VCR.use_cassette('IcelandicMembersPage', &test) }
 
-  url = 'http://www.althingi.is/thingmenn/althingismenn/'
+  subject do
+    url = 'http://www.althingi.is/thingmenn/althingismenn/'
+    IcelandicMembersPage.new(response: Scraped::Request.new(url: url).response)
+  end
 
   describe 'member_urls' do
     it 'should return the expected number of urls' do
-      IcelandicMembersPage.new(response: Scraped::Request.new(url: url)
-                                                         .response)
-                          .member_urls
-                          .count
-                          .must_equal 63
+      subject.member_urls.count.must_equal 63
     end
   end
 
   describe 'member_urls' do
     it 'should return an expected url in its result' do
-      IcelandicMembersPage.new(response: Scraped::Request.new(url: url)
-                                                         .response)
-                          .member_urls
-                          .must_include 'http://www.althingi.is/' \
-                                        'altext/cv/is/?nfaerslunr=1265'
+      wanted = 'http://www.althingi.is/altext/cv/is/?nfaerslunr=1265'
+      subject.member_urls.must_include wanted
     end
   end
 end
